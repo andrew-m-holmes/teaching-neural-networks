e663781a2ea5e1c8ebf665c11ae68aa2c4142366
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-12 22:05:23 -0400

    rm manim

--- manim-script/circle.py
+++
@@ -1,8 +0,0 @@
-import manim
-
-
-class CreateCircle(manim.Scene):
-    def construct(self):
-        circle = manim.Circle()
-        circle.set_fill(manim.RED, opacity=0.75)
-        self.play(manim.Create(circle))
