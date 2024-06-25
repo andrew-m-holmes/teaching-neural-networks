7e36cc01347901d19f32ba9eb713e9a89100721b
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-24 22:53:21 -0400

    starting code for landscape plot

--- scripts/landscape.py
+++ scripts/landscape.py
@@ -3,23 +3,14 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 
-class polymod:
-
-    def __init__(self, ndim: int) -> None:
-        self.coefficients = np.random.randint(-1, 1, ndim).astype(
-            float
-        ) * np.random.randn(ndim)
-
-    def __call__(self, input: np.ndarray) -> np.ndarray:
-        return np.sum(input * self.coefficients, axis=1)
+def polynomial(coefficients, x):
+    return sum(c * (x**i) for i, c in enumerate(coefficients))
 
 
-def main():
+num_coefficients = 5
+num_samples = 1000
+coefficients = np.random.uniform(-10, 10, (num_samples, num_coefficients))
+pca = PCA(n_components=2)
 
-    np.random.seed(0)
-    ndim = 5
-    data = np.random.randn(100, ndim)
-
-    pca = PCA(n_components=2, svd_solver="covariance_eigh")
-    model = polymod(ndim)
-    pca.fit(model.coefficients.reshape(1, -1))
+x = np.linspace(-1, 1, 100)
+loss = np.array([np.mean(polynomial(coeff, x) ** 2) for coeff in coefficients])
