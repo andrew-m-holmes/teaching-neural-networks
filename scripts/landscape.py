120dc5b31ceab85cb40d2fdbe1c2b8d5a0ff837b
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-26 22:41:34 -0400

    rm plot

--- scripts/landscape.py
+++ scripts/landscape.py
@@ -5,12 +5,3 @@ from sklearn.decomposition import PCA
 
 def polynomial(coefficients, x):
     return sum(c * (x**i) for i, c in enumerate(coefficients))
-
-
-num_coefficients = 5
-num_samples = 1000
-coefficients = np.random.uniform(-10, 10, (num_samples, num_coefficients))
-pca = PCA(n_components=2)
-
-x = np.linspace(-1, 1, 100)
-loss = np.array([np.mean(polynomial(coeff, x) ** 2) for coeff in coefficients])
