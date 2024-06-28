811b80e8e0db719acefc54da5f82afcf70f5c36c
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-27 20:34:40 -0400

    rm file

--- scripts/landscape.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-
-
-def polynomial(coefficients, x):
-    return sum(c * (x**i) for i, c in enumerate(coefficients))
