3efe19547ff207b221c468c9c45a4088be977e9a
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-27 22:48:37 -0400

    ResNet starter code

---
+++ scripts/resnet.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+class Block(nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
