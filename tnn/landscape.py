e75c5adc4680d537b4bbf7df72a44592a806e3f1
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-30 14:28:00 -0400

    add import

--- tnn/landscape.py
+++ tnn/landscape.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
+import torch.nn.functional as f
 from mpl_toolkits.mplot3d import Axes3D
 
 # Dummy dataset
