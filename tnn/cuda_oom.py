e80f15c367157943ad116491716512943c96b8ba
Andrew Holmes <andrewholmes011002@gmail.com>
2024-12-21 09:19:56 -0500

    new cuda oom error img and code

--- tnn/cuda_oom.py
+++ tnn/cuda_oom.py
@@ -2,13 +2,17 @@ import torch
 
 
 def main():
-    tensor = torch.randn(10000).double()
+    tensor = torch.randn(100000000).double()
     tensors = []
 
     while True:
         cloned = tensor.clone().to("cuda")
         tensors.append(cloned)
+        print(f"Allocated tensor on {cloned.device}, total tensors: {len(tensors)}")
 
 
 if __name__ == "__main__":
-    main()
+    if torch.cuda.is_available():
+        main()
+    else:
+        print("CUDA is not available.")
